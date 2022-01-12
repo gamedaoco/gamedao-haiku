82f1aa2b17f357b2679c21c6ebfb3c67e8827f4d
2075 <2075@zero.io>
2022-01-13 00:36:09 +0100

    chore: prettier

--- pkg/app/src/pages/_document.tsx
+++ pkg/app/src/pages/_document.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import createEmotionServer from '@emotion/server/create-instance'
-import createEmotionCache from '../theme/createEmotionCache'
-import theme from '../theme'
+import createEmotionCache from 'src/theme/createEmotionCache'
+import theme from 'src/theme'
 
 export default class MyDocument extends Document {
 	render() {
@@ -34,7 +34,10 @@ MyDocument.getInitialProps = async (ctx) => {
 
 	ctx.renderPage = () =>
 		originalRenderPage({
-			enhanceApp: (App) => (props) => <App emotionCache={cache} {...props} />,
+			enhanceApp: (App: any) =>
+				function EnhanceApp(props) {
+					return <App emotionCache={cache} {...props} />
+				},
 		})
 
 	const initialProps = await Document.getInitialProps(ctx)
