e79d2950f483b82e40f2c92e420ce7512313ff57
Persqual <pascal@zero.io>
2022-02-21 16:06:45 +0100

    Test

--- pkg/app/src/hooks/useApiProvider.ts
+++ pkg/app/src/hooks/useApiProvider.ts
@@ -1,7 +1,9 @@
 import { ApiProvider } from 'src/@types/network'
 import { useNetworkContext } from 'provider/network/modules/context'
+import f from '../../../translations/src/app/en-US.json'
 
 export function useApiProvider(): ApiProvider {
 	const { selectedApiProvider } = useNetworkContext()
+	console.log(f)
 	return selectedApiProvider
 }
