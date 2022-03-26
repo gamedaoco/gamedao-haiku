9bd1dfc78edc3820e82fcf3a0399f55bf6071592
Persqual <97976779+Persqual@users.noreply.github.com>
2022-03-26 16:19:31 +0100

    feat(organisation-index-example): added example how to retrieve data from graphql in frontend (#50)

--- pkg/app/src/pages/app/governance.tsx
+++ pkg/app/src/pages/app/governance.tsx
@@ -4,15 +4,36 @@ import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
 
 import { Layout } from 'src/layouts/default/layout'
+import { useBodiesQuery } from '@gamedao-haiku/graphql/dist'
+import { CircularProgress } from '@mui/material'
 
 export function GovernancePage() {
+	const { loading, data } = useBodiesQuery()
+
 	return (
 		<Layout showHeader showFooter showSidebar title="Governance">
-			<Box sx={{ p: '4rem', height: '90vh' }}>
+			<Box sx={{ p: '4rem', minHeight: '90vh' }}>
 				<Paper sx={{ p: '4rem', height: '100%', borderRadius: '.5rem' }} elevation={10}>
 					<Typography sx={{ fontWeight: '800' }} variant={'h2'}>
-						Hello.
+						Hello. Bodies count: {loading ? <CircularProgress /> : data?.bodies.length}
 					</Typography>
+					{data && (
+						<ul>
+							{data.bodies.map((body) => (
+								<li key={body.id}>
+									<div>Name: {body.name}</div>
+									<div>
+										Members:
+										<ul>
+											{body.members.map((member) => (
+												<li key={`${body.id}-${member.address}`}>{member.address}</li>
+											))}
+										</ul>
+									</div>
+								</li>
+							))}
+						</ul>
+					)}
 				</Paper>
 			</Box>
 		</Layout>
