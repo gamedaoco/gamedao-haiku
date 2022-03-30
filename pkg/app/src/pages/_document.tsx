9329983dc9a491c08c23c68894411dd74b5f43d9
DarkNebula0 <andre@schnelzer.io>
2022-03-30 11:55:39 +0200

    chore: remove css from head

--- pkg/app/src/pages/_document.tsx
+++ pkg/app/src/pages/_document.tsx
@@ -60,6 +60,7 @@ export default class MyDocument extends Document {
 						rel="stylesheet"
 						href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
 					/>
+					<link rel="stylesheet" href="/iconfont.css" />
 				</Head>
 				<body
 					style={{
