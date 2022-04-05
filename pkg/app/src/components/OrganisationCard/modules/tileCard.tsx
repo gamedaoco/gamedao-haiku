8bb008659e39884809c2c873ec4fa7632208d8fe
sniper365 <devtops2050@gmail.com>
2022-04-05 09:22:21 -0400

    chore(tileCard): modify loading curcular placed in card itself

--- pkg/app/src/components/OrganisationCard/modules/tileCard.tsx
+++ pkg/app/src/components/OrganisationCard/modules/tileCard.tsx
@@ -67,14 +67,6 @@ export const TileCard = ({ item, metadata }: ComponentsPros) => {
 		)
 	}, [item, address])
 
-	if (!item || !metadata) {
-		return (
-			<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-				<CircularProgress color="inherit" />
-			</Box>
-		)
-	}
-
 	return (
 		<NavLink href={`${toLink}${item.id}`}>
 			<Card
@@ -86,28 +78,36 @@ export const TileCard = ({ item, metadata }: ComponentsPros) => {
 					cursor: 'pointer',
 				}}
 			>
-				<CardHeader
-					avatar={
-						<Avatar src={`${gateway}${metadata?.logo}`} sx={{ width: 64, height: 64 }}>
-							{item?.name.slice(0, 1)}
-						</Avatar>
-					}
-					title={<Typography noWrap>{item?.name}</Typography>}
-					subheader={SubHeader}
-				/>
-				<CardContent>
-					<Typography
-						sx={{
-							overflow: 'hidden',
-							textOverflow: 'ellipsis',
-							display: '-webkit-box',
-							'-webkit-line-clamp': '2',
-							'-webkit-box-orient': 'vertical',
-						}}
-					>
-						{metadata?.description}
-					</Typography>
-				</CardContent>
+				{!item || !metadata ? (
+					<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+						<CircularProgress color="inherit" />
+					</Box>
+				) : (
+					<>
+						<CardHeader
+							avatar={
+								<Avatar src={`${gateway}${metadata?.logo}`} sx={{ width: 64, height: 64 }}>
+									{item?.name.slice(0, 1)}
+								</Avatar>
+							}
+							title={<Typography noWrap>{item?.name}</Typography>}
+							subheader={SubHeader}
+						/>
+						<CardContent>
+							<Typography
+								sx={{
+									overflow: 'hidden',
+									textOverflow: 'ellipsis',
+									display: '-webkit-box',
+									'-webkit-line-clamp': '2',
+									'-webkit-box-orient': 'vertical',
+								}}
+							>
+								{metadata?.description}
+							</Typography>
+						</CardContent>
+					</>
+				)}
 			</Card>
 		</NavLink>
 	)
