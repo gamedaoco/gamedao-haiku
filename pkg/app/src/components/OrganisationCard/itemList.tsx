6e310be4929af9da6b6c10cda47a1472a27cb663
developer-sidani <67205042+developer-sidani@users.noreply.github.com>
2022-04-27 11:55:31 +0300

    Organisations List Content Area (#112)
    * feat(page): organisation list page
    
     listed organisation list, added a search text field, create button
    
    * chore: added @mui/styles package
    
    * feat: item list component
    
    displaying 3 columns of the organisation card
    
    Co-authored-by: hadi-ibra <17hadiibrahim@gmail.com>

--- pkg/app/src/components/OrganisationCard/itemList.tsx
+++ pkg/app/src/components/OrganisationCard/itemList.tsx
@@ -16,14 +16,14 @@ export function ItemList({ items, loading }: ComponentProps) {
 		<Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
 			{loading ? (
 				[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((x) => (
-					<Grid item xs={4} sm={4} md={6} key={x}>
+					<Grid item xs={4} key={x}>
 						<LoadingTileCard />
 					</Grid>
 				))
 			) : (
 				<>
 					{items?.map((item, index) => (
-						<Grid item xs={4} sm={4} md={6} key={index}>
+						<Grid item xs={4} key={index}>
 							<TileCard item={item} />
 						</Grid>
 					))}
