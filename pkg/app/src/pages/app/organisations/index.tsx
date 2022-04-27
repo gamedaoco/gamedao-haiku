858180d45b2da92915d449efd57231b4dc24bdcc
hadi-ibra <17hadiibrahim@gmail.com>
2022-04-27 10:07:30 +0300

    feat(page): organisation list page
     listed organisation list, added a search text field, create button

--- pkg/app/src/pages/app/organisations/index.tsx
+++ pkg/app/src/pages/app/organisations/index.tsx
@@ -1,13 +1,44 @@
 import React, { useEffect } from 'react'
 import Box from '@mui/material/Box'
-import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
 import { ItemList } from 'components/OrganisationCard/itemList'
 import { Layout } from 'src/layouts/default/layout'
 import { Body, useBodiesQuery } from '@gamedao-haiku/graphql/dist'
-import { CircularProgress } from '@mui/material'
+import { Button, Container, createSvgIcon, Grid, InputAdornment, TextField } from '@mui/material'
+import { makeStyles } from '@mui/styles'
+
+const PlusIcon = createSvgIcon(
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+		<path
+			fillRule="evenodd"
+			d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+			clipRule="evenodd"
+		/>
+	</svg>,
+	'Plus',
+)
+
+const SearchIcon = createSvgIcon(
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+		={' '}
+		<path
+			fillRule="evenodd"
+			d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+			clipRule="evenodd"
+		/>
+	</svg>,
+	'Search',
+)
 
 export function OrganisationPage() {
+	const useStyles = makeStyles({
+		field: {
+			'& fieldset': {
+				borderRadius: 8,
+			},
+		},
+	})
+	const classes = useStyles()
 	const { loading, error, data } = useBodiesQuery()
 	useEffect(() => {
 		if (error) {
@@ -17,16 +48,66 @@ export function OrganisationPage() {
 
 	return (
 		<Layout showHeader showFooter showSidebar title="Organisation">
-			<Box sx={{ p: { sx: '.5rem', sm: '1rem', lg: '4rem' }, minHeight: '90vh' }}>
-				<Paper
-					sx={{ p: { sx: '.5rem', sm: '1rem', lg: '4rem' }, height: '100%', borderRadius: '.5rem' }}
-					elevation={10}
-				>
-					<Typography sx={{ fontWeight: '800' }} variant={'h2'}>
-						Hello. Bodies count: {loading ? <CircularProgress /> : data?.bodies.length}
-					</Typography>
+			<Box
+				component="main"
+				sx={{
+					flexGrow: 1,
+					py: 8,
+				}}
+			>
+				<Container maxWidth="xl">
+					<Box sx={{ mb: 4 }}>
+						<Grid container justifyContent="space-between" spacing={3}>
+							<Grid item>
+								<Typography variant="h3">Organisations</Typography>
+							</Grid>
+							<Grid item>
+								<Button
+									startIcon={<PlusIcon fontSize="small" />}
+									sx={{ color: '#919EAB', borderColor: '#919EAB', borderRadius: 2 }}
+									variant="outlined"
+								>
+									Create
+								</Button>
+							</Grid>
+						</Grid>
+						<Box
+							sx={{
+								m: -1,
+								mt: 3,
+							}}
+						>
+							<Box
+								sx={{
+									flexGrow: 1,
+									m: 1.5,
+								}}
+							>
+								<Grid container spacing={3}>
+									<Grid item xs={12} md={4}>
+										<TextField
+											id="outlined-basic"
+											size="small"
+											className={classes.field}
+											fullWidth
+											InputProps={{
+												startAdornment: (
+													<InputAdornment position="start">
+														<SearchIcon fontSize="small" />
+													</InputAdornment>
+												),
+											}}
+											placeholder="Search Organisations…"
+										/>
+									</Grid>
+									<Grid item xs={0} md={5}></Grid>
+									<Grid item xs={12} md={3}></Grid>
+								</Grid>
+							</Box>
+						</Box>
+					</Box>
 					<ItemList items={data?.bodies?.slice() as Body[]} loading={loading}></ItemList>
-				</Paper>
+				</Container>
 			</Box>
 		</Layout>
 	)
