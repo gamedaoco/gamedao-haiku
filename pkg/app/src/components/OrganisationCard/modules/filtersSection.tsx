e73cfa767256a6ef086b1b951e65acd95e4d4886
developer-sidani <developer.sidani@gmail.com>
2022-05-04 01:11:01 +0300

    fix: replaced options to MenuItem component

--- pkg/app/src/components/OrganisationCard/modules/filtersSection.tsx
+++ pkg/app/src/components/OrganisationCard/modules/filtersSection.tsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react'
 import Box from '@mui/material/Box'
-import { createSvgIcon, FormControl, Grid, InputAdornment, Select, SelectChangeEvent, TextField } from '@mui/material'
+import {
+	createSvgIcon,
+	FormControl,
+	Grid,
+	InputAdornment,
+	MenuItem,
+	Select,
+	SelectChangeEvent,
+	TextField,
+} from '@mui/material'
 import Typography from '@mui/material/Typography'
 import FilterListIcon from '@mui/icons-material/FilterList'
 import { makeStyles } from '@mui/styles'
 
 const SearchIcon = createSvgIcon(
 	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-		={' '}
 		<path
 			fillRule="evenodd"
 			d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
@@ -113,9 +121,9 @@ const FiltersSection = () => {
 									>
 										<Select value={sortOption} onChange={handleChange}>
 											{sortOptions.map((x) => (
-												<option value={x.value} key={x.value}>
+												<MenuItem value={x.value} key={x.value}>
 													{x.name}
-												</option>
+												</MenuItem>
 											))}
 										</Select>
 									</FormControl>
