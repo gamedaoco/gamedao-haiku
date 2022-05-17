40dd27129a2bdf12df19c0c6556256a7c664ddfb
developer-sidani <developer.sidani@gmail.com>
2022-05-17 12:18:12 +0300

    feat(page): initiating Account Page

---
+++ pkg/app/src/pages/app/account.tsx
@@ -0,0 +1,12 @@
+import { Layout } from 'layouts/default/layout'
+import { NextPage } from 'next'
+
+const AccountPage: NextPage = () => {
+	return (
+		<Layout showHeader showSidebar title="My Account">
+			<div>Hello</div>
+		</Layout>
+	)
+}
+
+export default AccountPage
