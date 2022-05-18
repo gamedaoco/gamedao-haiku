18d28d6feddd847456170361dbeda9b4d7bc514a
developer-sidani <developer.sidani@gmail.com>
2022-05-18 10:12:10 +0300

    fix: checking if the wallet is connected in the account page

--- pkg/app/src/pages/app/account/index.tsx
+++ pkg/app/src/pages/app/account/index.tsx
@@ -1,10 +1,14 @@
 import AccountPageGeneralLayout from 'components/Account/general-layout'
+import { NoWalletConnected } from 'components/NoWalletConnected/noWalletConnected'
 import { useCurrentAccountState } from 'hooks/useCurrentAccountState'
 import { Layout } from 'layouts/default/layout'
 import { NextPage } from 'next'
 
 const AccountPage: NextPage = () => {
 	const accountState = useCurrentAccountState()
+	if (!accountState) {
+		return <NoWalletConnected />
+	}
 
 	return (
 		<Layout showHeader showSidebar title="My Account">
