7fe8041f4c8fd7a768ec328c87627ff0aa57038c
hadi-ibra <17hadiibrahim@gmail.com>
2022-05-19 10:41:41 +0300

    feat: added collectabes tab component, 0:30h
    The collectables tab contains collectables list component.
    
    Reviewed-by: 5-mark
    Refs: [Account] Collectables #145

--- pkg/app/src/components/Account/modules/my-collectables-tab.tsx
+++ pkg/app/src/components/Account/modules/my-collectables-tab.tsx
@@ -1,11 +1,40 @@
-import React, { FC, memo } from 'react'
+import { Paper, Stack, Typography } from '@mui/material'
+import React, { FC, memo, useEffect } from 'react'
 import { AccountState } from 'src/@types/extension'
+import Script from 'next/script'
+import { useCollectablesForUserLazyQuery } from '@gamedao-haiku/graphql/dist'
+import { getKusamaAddressFromAccountState } from 'src/utils/accountUtils'
+import CollectablesList from './collectables-section/collectables-list'
 
 interface MyCollectablesTabProps {
 	accountState: AccountState
 }
+
 const MyCollectablesTab: FC<MyCollectablesTabProps> = ({ accountState }) => {
-	return <div>my-collectables-tab</div>
+	const [loadCollectables, { loading, data }] = useCollectablesForUserLazyQuery()
+
+	useEffect(() => {
+		if (accountState) {
+			loadCollectables({ variables: { owner: getKusamaAddressFromAccountState(accountState) } })
+		}
+	}, [accountState])
+
+	return (
+		<>
+			<Script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js" />
+			<Stack
+				component={Paper}
+				spacing={0.5}
+				padding={3}
+				borderRadius="16px"
+				//TODO: Change the static color to fetch from theme once the last pull request is merged
+				style={{ backgroundColor: '#212B36' }}
+			>
+				<Typography variant="h6">My Collectables </Typography>
+				<CollectablesList loading={loading} items={data} />
+			</Stack>
+		</>
+	)
 }
 
 export default memo(MyCollectablesTab)
