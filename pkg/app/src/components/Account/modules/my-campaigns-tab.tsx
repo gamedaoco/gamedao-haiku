b9b2c009fbc9cab59a0f624c33460639502f6b44
hadi-ibra <17hadiibrahim@gmail.com>
2022-05-25 17:46:27 +0300

    feat: add campaigns tab content

--- pkg/app/src/components/Account/modules/my-campaigns-tab.tsx
+++ pkg/app/src/components/Account/modules/my-campaigns-tab.tsx
@@ -1,11 +1,19 @@
+import { Box } from '@mui/material'
 import React, { FC, memo } from 'react'
 import { AccountState } from 'src/@types/extension'
+import ContributedCampaginsSection from './campaigns-section/contributed-campaigns-section'
+import CreatedCampaignSection from './campaigns-section/created-campaigns-section/created-campains-section'
 
 interface MyCampaignsTabProps {
 	accountState: AccountState
 }
 const MyCampaignsTab: FC<MyCampaignsTabProps> = ({ accountState }) => {
-	return <div>my-campaigns-tab</div>
+	return (
+		<Box>
+			<CreatedCampaignSection />
+			<ContributedCampaginsSection />
+		</Box>
+	)
 }
 
 export default memo(MyCampaignsTab)
