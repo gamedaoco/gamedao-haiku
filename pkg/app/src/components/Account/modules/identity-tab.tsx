aa86d574148a6cb62c28c7f99a709ff4ad89a71b
developer-sidani <developer.sidani@gmail.com>
2022-05-25 21:51:30 +0300

    feat(account-identity): fetch identity from account address
    enableReinitialize is now active from formik to replace the fetched data from the account

--- pkg/app/src/components/Account/modules/identity-tab.tsx
+++ pkg/app/src/components/Account/modules/identity-tab.tsx
@@ -1,3 +1,4 @@
+import { useIdentityByAddress } from 'hooks/useIdentityByAddress'
 import React, { FC, memo } from 'react'
 import { AccountState } from 'src/@types/extension'
 import IdentityForm from './identity-section/form'
@@ -6,7 +7,10 @@ interface IdentityTabProps {
 	accountState: AccountState
 }
 const IdentityTab: FC<IdentityTabProps> = ({ accountState }) => {
-	return <IdentityForm />
+	// todo - replace the static identity with the address from the accountstate when merged
+	const { identity } = useIdentityByAddress('5Dkv3jCJqDdk2uWWdT2DNXAFFgTLhHRZxdFmU8LHz9R74BeS')
+
+	return <IdentityForm identity={identity} />
 }
 
 export default memo(IdentityTab)
