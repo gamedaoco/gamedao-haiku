536f84e0428a62dc17a0f6997b5faa246340c770
developer-sidani <developer.sidani@gmail.com>
2022-06-13 15:05:46 +0300

    fix(features-toggle): features hook type, 0:10h;

--- pkg/app/src/hooks/useFeatures.ts
+++ pkg/app/src/hooks/useFeatures.ts
@@ -2,5 +2,5 @@ import type { Features } from '@gamedao-haiku/graphql/dist/types'
 import { useAppContext } from 'provider/app/modules/context'
 
 export function useFeatures(): Features {
-	return useAppContext()?.features
+	return useAppContext()?.features as Features
 }
