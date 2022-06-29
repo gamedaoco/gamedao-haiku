65ceb8bb9f8a9d1e6455957bb03008494b9b5061
Pascal <45153452+PAkhidue@users.noreply.github.com>
2022-06-29 20:58:34 +0200

    fix(proposal-data): added general proposal data (#214)

--- pkg/squid/src/model/generated/_proposalTypeWithdrawalData.ts
+++ pkg/squid/src/model/generated/_proposalTypeWithdrawalData.ts
@@ -3,13 +3,24 @@ import * as marshal from './marshal';
 
 export class ProposalTypeWithdrawalData {
 	public readonly isTypeOf = 'ProposalTypeWithdrawalData';
+	private _type!: number;
 	private _withdrawAmount!: bigint;
 
 	constructor(props?: Partial<Omit<ProposalTypeWithdrawalData, 'toJSON'>>, json?: any) {
 		Object.assign(this, props);
 		if (json != null) {
+			this._type = marshal.int.fromJSON(json.type);
 			this._withdrawAmount = marshal.bigint.fromJSON(json.withdrawAmount);
 		}
+	}
+
+	get type(): number {
+		assert(this._type != null, 'uninitialized access');
+		return this._type;
+	}
+
+	set type(value: number) {
+		this._type = value;
 	}
 
 	get withdrawAmount(): bigint {
@@ -24,6 +35,7 @@ export class ProposalTypeWithdrawalData {
 	toJSON(): object {
 		return {
 			isTypeOf: this.isTypeOf,
+			type: this.type,
 			withdrawAmount: marshal.bigint.toJSON(this.withdrawAmount),
 		};
 	}
