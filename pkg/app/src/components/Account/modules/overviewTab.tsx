cfa83551ec17c977cf88adac7e9c8efd6352cf6f
developer-sidani <67205042+developer-sidani@users.noreply.github.com>
2022-07-07 12:52:28 +0300

    feat: balances subscription (#243)
    * feat: balances subscription
    
    * fix: balance subscription
    
    * fix: remove loading state from balances
    
    * fix: display with existing symbols

--- pkg/app/src/components/Account/modules/overviewTab.tsx
+++ pkg/app/src/components/Account/modules/overviewTab.tsx
@@ -1,23 +1,16 @@
-import React, { FC, memo } from 'react'
+import React from 'react'
 
 import { Box, Grid } from '@mui/material'
-import { AccountState } from 'src/@types/extension'
+import { useCurrentAccountAddress } from 'hooks/useCurrentAccountAddress'
 import { Organization, useAccountOrganizationsSubscription } from 'src/queries'
-import { getAddressFromAccountState } from 'src/utils/accountUtils'
-
-import { balances } from 'components/Account/mock/balances'
 
 import MyOrganisationsTable from './MyOrganisations/myOrganisations'
 import MyAchievementsCard from './OverviewSection/myAchievements'
-import MyBalancesCard from './OverviewSection/myBalances'
-import MyCollectablesTab from './myCollectablesTab'
+import { MyBalancesCard } from './OverviewSection/myBalances'
+import { MyCollectablesTab } from './myCollectablesTab'
 
-interface OverviewTabProps {
-	accountState: AccountState
-}
-
-const OverviewTab: FC<OverviewTabProps> = ({ accountState }) => {
-	const address = getAddressFromAccountState(accountState)
+export function OverviewTab() {
+	const address = useCurrentAccountAddress()
 	const { data, loading } = useAccountOrganizationsSubscription({
 		variables: {
 			address: address,
@@ -31,7 +24,7 @@ const OverviewTab: FC<OverviewTabProps> = ({ accountState }) => {
 					<MyAchievementsCard />
 				</Grid>
 				<Grid item xs={12} md={8}>
-					<MyBalancesCard balances={balances} loading={loading} />
+					<MyBalancesCard />
 				</Grid>
 				<Grid item xs={12}>
 					<MyOrganisationsTable
@@ -41,11 +34,9 @@ const OverviewTab: FC<OverviewTabProps> = ({ accountState }) => {
 					/>
 				</Grid>
 				<Grid item xs={12}>
-					<MyCollectablesTab accountState={accountState} />
+					<MyCollectablesTab />
 				</Grid>
 			</Grid>
 		</Box>
 	)
 }
-
-export default memo(OverviewTab)
--- pkg/app/src/components/Account/sectionsLayout.tsx
+++ pkg/app/src/components/Account/sectionsLayout.tsx
@@ -5,9 +5,9 @@ import { AccountState } from 'src/@types/extension'
 
 import IdentityTab from './modules/identityTab'
 import MyCampaignsTab from './modules/myCampaignsTab'
-import MyCollectablesTab from './modules/myCollectablesTab'
+import { MyCollectablesTab } from './modules/myCollectablesTab'
 import MyOrganisationsTab from './modules/myOrganisationsTab'
-import OverviewTab from './modules/overviewTab'
+import { OverviewTab } from './modules/overviewTab'
 
 interface SectionsLayoutProps {
 	accountState: AccountState
@@ -16,10 +16,10 @@ interface SectionsLayoutProps {
 const SectionsLayout: FC<SectionsLayoutProps> = ({ accountState, currentTab }) => {
 	return (
 		<>
-			{currentTab === AccountTabs.OVERVIEW && <OverviewTab accountState={accountState} />}
+			{currentTab === AccountTabs.OVERVIEW && <OverviewTab />}
 			{currentTab === AccountTabs.ORGANIZATIONS && <MyOrganisationsTab accountState={accountState} />}
 			{currentTab === AccountTabs.CAMPAIGNS && <MyCampaignsTab accountState={accountState} />}
-			{currentTab === AccountTabs.COLLECTABLES && <MyCollectablesTab accountState={accountState} />}
+			{currentTab === AccountTabs.COLLECTABLES && <MyCollectablesTab />}
 			{currentTab === AccountTabs.IDENTITY && <IdentityTab accountState={accountState} />}
 		</>
 	)
