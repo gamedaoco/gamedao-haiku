5bd1d58482fa4fe5248aa2259087052ff768bd9b
hadi-ibra <17hadiibrahim@gmail.com>
2022-07-11 17:53:24 +0300

    feat: add campaign status enum, 0:05h

---
+++ pkg/app/src/@types/campaignStatus.ts
@@ -0,0 +1,10 @@
+export enum CampaignStatus {
+	INIT = 'Init',
+	ACTIVE = 'Active',
+	PAUSED = 'Paused',
+	FINALIZING = 'Finalizing',
+	REVERTING = 'Reverting',
+	SUCCESS = 'Success',
+	FAILED = 'Failed',
+	LOCKED = 'Locked',
+}
