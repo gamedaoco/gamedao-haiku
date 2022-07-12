f0e45449b884d5671b579e1848a9ae61f4c642c9
developer-sidani <developer.sidani@gmail.com>
2022-07-12 14:34:01 +0300

    fix: campaigns pagination props

--- pkg/app/src/components/Account/modules/myCampaignsTab.tsx
+++ pkg/app/src/components/Account/modules/myCampaignsTab.tsx
@@ -1,26 +1,27 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 
 import { Box } from '@mui/material'
-import { useCampaignContributorsSubscription } from 'src/queries'
+import { useCurrentAccountState } from 'hooks/useCurrentAccountState'
+import { Campaign, useCampaignContributorsSubscription } from 'src/queries'
 import { useCampaignSubscription } from 'src/queries'
 import { getAddressFromAccountState } from 'src/utils/accountUtils'
 
 import { CreatedCampaignSection } from './CampaignsSection/CreatedCampaignsSection/createdCampainsSection'
 import ContributedCampaignsSection from './CampaignsSection/contributedCampaignsSection/contributedCampaignsSection'
-import { useCurrentAccountState } from 'hooks/useCurrentAccountState'
 
 export function MyCampaignsTab() {
 	const accountState = useCurrentAccountState()
 	const { data, loading } = useCampaignSubscription({
 		variables: { address: getAddressFromAccountState(accountState) },
 	})
+	const paginatedData = useMemo<Campaign[]>(() => data?.campaign?.slice() as Campaign[], [])
 	const { data: campaignContributorsData, loading: campaignContributorsLoading } =
 		useCampaignContributorsSubscription({
 			variables: { address: getAddressFromAccountState(accountState) },
 		})
 	return (
 		<Box>
-			<CreatedCampaignSection data={data} loading={loading} />
+			<CreatedCampaignSection data={paginatedData} loading={loading} />
 			<ContributedCampaignsSection data={campaignContributorsData} loading={campaignContributorsLoading} />
 		</Box>
 	)
