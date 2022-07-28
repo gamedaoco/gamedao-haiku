e2a6b6e3d1eb96f09a69d8f5ba6d1dedc5997204
DarkNebula0 <andre@schnelzer.io>
2022-07-28 16:48:27 +0200

    feat(dev-env): add create campaign to script

--- pkg/dev-env/testConsole.js
+++ pkg/dev-env/testConsole.js
@@ -1,5 +1,4 @@
 const { Blob } = require('node:buffer')
-
 const { faker } = require('@faker-js/faker')
 const { ApiPromise, WsProvider } = require('@polkadot/api')
 const { Keyring } = require('@polkadot/keyring')
@@ -7,10 +6,23 @@ const { cryptoWaitReady } = require('@polkadot/util-crypto')
 const fetch = require('node-fetch')
 const readline = require('readline')
 
+const blockTime = 3
+const blocksPerDay = (24 * 60 * 60) / blockTime
+const blockFactor = blockTime * (60 / blockTime) * 60 * 24
+
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout,
 })
+
+async function updateBalance(api, signer, address) {
+	await api.tx.sudo.sudo(api.tx.currencies.updateBalance(address, 0, '100000000000000000000')).signAndSend(signer)
+	await sleep(4000)
+	await api.tx.sudo.sudo(api.tx.currencies.updateBalance(address, 1, '100000000000000000000')).signAndSend(signer)
+	await sleep(4000)
+	await api.tx.sudo.sudo(api.tx.currencies.updateBalance(address, 2, '100000000000000000000')).signAndSend(signer)
+	await sleep(4000)
+}
 
 async function uploadFileToIpfs(file) {
 	const { create } = await import('ipfs-http-client')
@@ -41,6 +53,8 @@ function sleep(ms) {
 	})
 }
 
+let currentBlockNumber = 0
+
 async function main() {
 	const wsProvider = new WsProvider('ws://localhost:9944')
 	const api = await ApiPromise.create({ provider: wsProvider })
@@ -49,28 +63,16 @@ async function main() {
 	const keyring = new Keyring({ type: 'sr25519', ss58Format: 25 })
 	const alice = keyring.addFromUri('//Alice')
 	const bob = keyring.addFromUri('//Bob')
+
+	api.derive.chain.bestNumberFinalized((result) => {
+		currentBlockNumber = result.toNumber()
+	})
+
 	console.log('Init accounts')
 	console.log(`Update alice balances`)
-	await api.tx.sudo
-		.sudo(api.tx.currencies.updateBalance(alice.address, 0, '100000000000000000000'))
-		.signAndSend(alice)
-	await sleep(4000)
-	await api.tx.sudo
-		.sudo(api.tx.currencies.updateBalance(alice.address, 1, '100000000000000000000'))
-		.signAndSend(alice)
-	await sleep(4000)
-	await api.tx.sudo
-		.sudo(api.tx.currencies.updateBalance(alice.address, 2, '100000000000000000000'))
-		.signAndSend(alice)
-	await sleep(4000)
-
+	await updateBalance(api, alice, alice.address)
 	console.log(`Update bob balances`)
-	await api.tx.sudo.sudo(api.tx.currencies.updateBalance(bob.address, 0, '100000000000000000000')).signAndSend(alice)
-	await sleep(4000)
-	await api.tx.sudo.sudo(api.tx.currencies.updateBalance(bob.address, 1, '100000000000000000000')).signAndSend(alice)
-	await sleep(4000)
-	await api.tx.sudo.sudo(api.tx.currencies.updateBalance(bob.address, 2, '100000000000000000000')).signAndSend(alice)
-	await sleep(4000)
+	await updateBalance(api, alice, bob.address)
 
 	while (true) {
 		const command = await new Promise((res) => rl.question('Please enter command: ', res))
@@ -94,6 +96,29 @@ async function main() {
 			for (let i = 0; i < amount; i++) {
 				await createDao(api, signer)
 			}
+		} else if (command.startsWith('createcampaign') || command.startsWith('cc')) {
+			if (params.length !== 3 && params.length !== 4) {
+				console.log('Invalid amount of params! Use command help for more information.')
+			}
+
+			const signer = params[1] === 'alice' ? alice : params[1] === 'bob' ? bob : null
+			if (!signer) {
+				console.log('Invalid signer! Use command help for more information.')
+			}
+
+			const daoHash = params[2]
+
+			if (!daoHash || !daoHash.startsWith('0x')) {
+				{
+					console.log('Invalid dao hash! Use command help for more information.')
+				}
+			}
+
+			const amount = params[3] ?? 1
+
+			for (let i = 0; i < amount; i++) {
+				await createCampaign(api, signer, daoHash)
+			}
 		}
 	}
 }
@@ -101,6 +126,7 @@ async function main() {
 async function help() {
 	console.log('Available commands:')
 	console.log('- cretedao/cd <Signer alice | bob> <amount optional>')
+	console.log('- createcampaign/cc <Signer alice | bob> <daohash | hash> <amount optional>')
 }
 
 async function createDao(api, signer) {
@@ -158,6 +184,75 @@ async function createDao(api, signer) {
 		.signAndSend(signer)
 	await sleep(4000)
 	console.log(`Created dao tx hash: ${hash.toString()}`)
+}
+
+async function createCampaign(api, signer, daoHash) {
+	const treasuryAddress = (await api.query.control.orgTreasury(daoHash)).toHuman()
+	console.log(`Treasury address: ${treasuryAddress}`)
+	console.log('Update treasury balance')
+	await updateBalance(api, signer, treasuryAddress)
+
+	const headerImage = await fetch('https://picsum.photos/1024/576')
+	const headerCID = await uploadFileToIpfs(new Blob([await headerImage.buffer()], { type: 'image/jpeg' }))
+
+	if (!headerCID) {
+		console.log('Image upload failed')
+		return
+	}
+	const name = faker.commerce.productName()
+
+	const file = new Blob(
+		[
+			JSON.stringify({
+				name: name,
+				markdown: faker.commerce.productDescription(),
+				description: faker.commerce.productDescription(),
+				logo: '',
+				title: '',
+				header: headerCID,
+				email: '',
+			}),
+		],
+		{ type: 'text/plain' },
+	)
+
+	const metadataCid = await uploadFileToIpfs(file)
+	if (!metadataCid) {
+		console.log('Metadata upload failed')
+		return
+	}
+
+	const mappedData = {
+		orgId: daoHash,
+		adminId: signer.address,
+		name: name,
+		target: '10000000000000000000',
+		deposit: '1000000000000000000',
+		expiry: currentBlockNumber + blocksPerDay,
+		protocol: 0,
+		governance: 0,
+		cid: metadataCid,
+		tokenSymbol: 'PLAY',
+		tokenName: 'PLAY',
+	}
+
+	const hash = await api.tx.flow
+		.createCampaign(
+			mappedData.orgId,
+			mappedData.adminId,
+			mappedData.name,
+			mappedData.target,
+			mappedData.deposit,
+			mappedData.expiry,
+			mappedData.protocol,
+			mappedData.governance,
+			mappedData.cid,
+			mappedData.tokenSymbol,
+			mappedData.tokenName,
+		)
+		.signAndSend(signer)
+	await sleep(4000)
+	console.log(`Campaign created tx hash: ${hash.toString()}`)
 }
 
 main()
