746abd690bd43e2cc856a683f98c2e66c286d7b8
hadi-ibra <17hadiibrahim@gmail.com>
2022-08-01 13:54:06 +0300

    fix: merge main

--- pkg/app/src/components/TabPanels/Organization/tmpOverview.tsx
+++ pkg/app/src/components/TabPanels/Organization/tmpOverview.tsx
@@ -73,11 +73,8 @@ export function TmpOverview() {
 				<Stack direction={{ xs: 'column', md: 'row' }} spacing={{ xs: 2, md: 4 }}>
 					<Paper sx={{ width: '100%' }}>
 						<Stack height="100%" spacing={1} padding={{ xs: 2, md: 4 }}>
-							<Typography variant="h6">Complete your Organization</Typography>
-							<Typography variant="body2">
-								To start using your oganization and deploy it on the chain, please complete following
-								steps and earn experience points!
-							</Typography>
+							<Typography variant="h6">{t('label:complete_your_organization')}</Typography>
+							<Typography variant="body2">{t('label:tmp_organization_description')}</Typography>
 							<Timeline position="right">
 								<TimelineItem sx={{ '&:before': { display: 'none' } }}>
 									<TimelineSeparator>
@@ -87,7 +84,7 @@ export function TmpOverview() {
 									<TimelineContent>
 										<Stack direction="row" justifyContent="space-between" height="100%">
 											<Typography variant="subtitle2" sx={{ textDecoration: 'line-through' }}>
-												Create organization
+												{t('transactions:createOrganization:title')}
 											</Typography>
 											<Typography variant="subtitle2">+15XP</Typography>
 										</Stack>
@@ -108,7 +105,7 @@ export function TmpOverview() {
 															: 'unset',
 													}}
 												>
-													Add a logo and banner
+													{t('label:add_logo_and_banner')}
 												</Typography>
 												<Typography
 													variant="body2"
@@ -118,7 +115,7 @@ export function TmpOverview() {
 															: 'unset',
 													}}
 												>
-													Click on the placeholder to upload an image
+													{t('label:click_placeholder_upload')}
 												</Typography>
 											</Stack>
 											<Typography variant="subtitle2">+15XP</Typography>
@@ -135,12 +132,16 @@ export function TmpOverview() {
 									<TimelineContent>
 										<Stack direction="row" justifyContent="space-between">
 											<Stack spacing={1}>
-												<Typography variant="subtitle2">Save organization on chain</Typography>
+												<Typography variant="subtitle2">
+													{t('label:save_organization_on_chain')}
+												</Typography>
 												<Typography variant="body2">
-													Deploy your organization onto the chain
+													{t('label:deploy_organization_to_chain')}
 												</Typography>
 											</Stack>
-											<Typography variant="subtitle2">Receive Total 50XP</Typography>
+											<Typography variant="subtitle2">
+												{t('label:total_received_xp', { xp: 50 })}
+											</Typography>
 										</Stack>
 									</TimelineContent>
 								</TimelineItem>
@@ -181,8 +182,7 @@ export function TmpOverview() {
 								DAO Padawan
 							</Typography>
 							<Typography variant="caption" textAlign="center">
-								Your organization is currently on the level Padavan. Reach another 40XP to reach the
-								next level!
+								{t('label:organization_level_description', { xp: 40 })}
 							</Typography>
 						</Stack>
 					</Paper>
@@ -192,7 +192,7 @@ export function TmpOverview() {
 						<Stack direction="row" alignItems="center" spacing={2}>
 							<Info />
 							<Typography>
-								Deploy organization on chain A min deposit of 5 GAME is needed.{' '}
+								{t('label:deploy_organization_min_deposit', { value: 5 })}
 								<Link
 									textAlign="center"
 									href="https://discord.com/channels/273529551483699200/772045307021885452"
@@ -201,7 +201,7 @@ export function TmpOverview() {
 									underline="always"
 									sx={{ whiteSpace: 'nowrap' }}
 								>
-									Get GAME here.
+									{t('label:get_token_here', { token: 'GAME' })}
 								</Link>
 							</Typography>
 						</Stack>
@@ -228,7 +228,7 @@ export function TmpOverview() {
 								disabled={!tx}
 								onClick={handleModalOpen}
 							>
-								Deploy Organization
+								{t('label:deploy_organization')}
 							</Button>
 						</Stack>
 					</Stack>
