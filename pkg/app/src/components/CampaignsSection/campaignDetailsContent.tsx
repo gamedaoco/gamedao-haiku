11a0ff264e78e18a3d658d126418dd131e854cbd
developer-sidani <developer.sidani@gmail.com>
2022-08-01 11:48:57 +0300

    fix: campaignDetails progress

--- pkg/app/src/components/CampaignsSection/campaignDetailsContent.tsx
+++ pkg/app/src/components/CampaignsSection/campaignDetailsContent.tsx
@@ -49,7 +49,10 @@ export function CampaignDetailsContent({
 	const theme = useTheme()
 	const systemProperties = useSystemProperties()
 	const blockNumber = useBlockNumber()
-	const progress = useMemo(() => getCampaignProgress(target, contributed), [contributed, target])
+	const progress = useMemo(
+		() => (getCampaignProgress(target, contributed) > 100 ? 100 : getCampaignProgress(target, contributed)),
+		[contributed, target],
+	)
 	const duration =
 		useMemo(
 			() => getTimeFromBlock(blockNumber, expiry, systemProperties?.blockTargetTime),
