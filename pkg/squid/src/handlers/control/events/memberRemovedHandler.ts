04e2cb1193dcd5a738982b1db7ae1443d2c4a1c7
Volodymyr Brazhnyk <crypto@vovacha.com>
2022-09-28 22:54:09 +0200

    feat: update squid to v61

--- pkg/squid/src/handlers/control/events/memberRemovedHandler.ts
+++ pkg/squid/src/handlers/control/events/memberRemovedHandler.ts
@@ -7,12 +7,12 @@ import { EventHandlerContext } from '@subsquid/substrate-processor';
 async function handleMemberRemovedEvent(context: EventHandlerContext) {
 	let eventName = 'Control.MemberAdded';
 	let raw_event = new ControlMemberRemovedEvent(context);
-	if (!raw_event.isV60) {
+	if (!raw_event.isV61) {
 		console.error(`Unknown version: ${eventName}`);
 		return;
 	}
 	let store = context.store;
-	let event = raw_event.asV60;
+	let event = raw_event.asV61;
 
 	let orgId = hashToHexString(event.orgId);
 	let address = addressCodec.encode(event.who);
