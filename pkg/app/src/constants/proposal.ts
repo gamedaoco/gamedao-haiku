845f5e06dd4d4f3d6e798d972f08b2321f70daa3
Pascal <45153452+PAkhidue@users.noreply.github.com>
2022-09-30 17:51:00 +0200

    feat(update-proposal-extrinsics): updated proposal and vote extrinsic

---
+++ pkg/app/src/constants/proposal.ts
@@ -0,0 +1,17 @@
+export const PROPOSAL_TYPES = {
+	0: 'General',
+	1: 'Withdrawal',
+	2: 'Spending',
+}
+
+export const PROPOSAL_MAJORITIES = {
+	0: 'Simple',
+	1: 'Relative',
+	2: 'Absolute',
+}
+
+export const PROPOSAL_UNITS = {
+	0: 'Account', // General
+	1: 'Account', // Withdrawal
+	2: 'Token', // Spending
+}
