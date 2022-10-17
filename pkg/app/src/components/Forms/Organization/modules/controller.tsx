45ecbd85299070f4e445f1ffe1983e54887e555f
2075 <2075@zero.io>
2022-10-17 14:05:26 +0200

    feat: udpate forms

--- pkg/app/src/components/Forms/Organization/modules/controller.tsx
+++ pkg/app/src/components/Forms/Organization/modules/controller.tsx
@@ -1,4 +1,5 @@
 import { Person } from '@mui/icons-material'
+import { Group } from '@mui/icons-material'
 
 import { BaseForm } from 'components/Forms/baseForm'
 import { RadioItem } from 'components/Forms/modules/radioItem'
@@ -8,22 +9,27 @@ interface ComponentProps {
 	setSelected: (number) => void
 }
 
+/*
+	INFO: currently type 1 and 3 are dysfunctional / disabled
+	value is not a counter but an int representing an enum
+*/
+
 export function Controller({ selected, setSelected }: ComponentProps) {
 	return (
 		<BaseForm title={'Who will control the organization?'}>
 			<RadioItem
 				icon={<Person sx={{ width: '40px', height: '40px' }} />}
-				title={'One Entity'}
+				title={'Single Entity'}
 				description={'A single entity can take decisions on behalf of the organization.'}
 				value={0}
 				selectedValue={selected}
 				onChange={setSelected}
 			/>
 			<RadioItem
-				icon={<Person sx={{ width: '40px', height: '40px' }} />}
+				icon={<Group sx={{ width: '40px', height: '40px' }} />}
 				title={'Community'}
 				description={'The community will take decisions together as a group.'}
-				value={1}
+				value={2}
 				selectedValue={selected}
 				onChange={setSelected}
 			/>
