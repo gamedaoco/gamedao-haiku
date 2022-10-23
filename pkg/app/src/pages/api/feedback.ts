4d418741e481507b2f60b05ed88b88d1a72a117f
2075 <2075@zero.io>
2022-10-23 19:57:49 +0200

    feat(feedback): add discord api

---
+++ pkg/app/src/pages/api/feedback.ts
@@ -0,0 +1,33 @@
+// curl -X POST localhost:3000/api/feedback -H "Content-Type:application/json;charset=utf-8" -d '{"msg":"hello"}'
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+import Discord from 'discord.js'
+import { Logger } from 'lib/logger'
+
+const log = Logger('API')
+
+const ID = process.env.DISCOID
+const TOKEN = process.env.DISCOTOKEN
+const discord = new Discord.WebhookClient(ID, TOKEN)
+
+export default async (
+	req: NextApiRequest,
+	res: NextApiResponse
+) => {
+
+	const { method, msg } = req.body
+
+	if ( method === 'POST' && msg !== null ) {
+
+		discord.send(msg)
+		log.info(`message sent`)
+		res.status(200).send('ok')
+
+	} else {
+
+		log.error( 'unauthorized' )
+		res.status(400).send( 'unauthorized' )
+
+	}
+
+}
