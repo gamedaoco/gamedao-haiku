abb2750c1e2686620e9eb23e6ae8deafc0943efc
Alexis <114652006+AlexisZero@users.noreply.github.com>
2022-10-28 22:12:58 +0200

    fix: fix community organizations creation (#466)
    Co-authored-by: Alexis Munoz <alexismn86@gmail.com>

--- pkg/app/src/components/Forms/Organization/modules/controller.tsx
+++ pkg/app/src/components/Forms/Organization/modules/controller.tsx
@@ -2,6 +2,7 @@ import { Person } from '@mui/icons-material'
 
 import { BaseForm } from 'components/Forms/baseForm'
 import { RadioItem } from 'components/Forms/modules/radioItem'
+import { useTranslation } from 'react-i18next'
 
 interface ComponentProps {
 	selected: number
@@ -9,21 +10,23 @@ interface ComponentProps {
 }
 
 export function Controller({ selected, setSelected }: ComponentProps) {
+	const { t } = useTranslation()
+
 	return (
 		<BaseForm title={'Who will control the organization?'}>
 			<RadioItem
 				icon={<Person sx={{ width: '40px', height: '40px' }} />}
-				title={'One Entity'}
-				description={'A single entity can take decisions on behalf of the organization.'}
+				title={t('page:organisations:settings:control:radio_button_entity:title')}
+				description={t('page:organisations:settings:control:radio_button_entity:description')}
 				value={0}
 				selectedValue={selected}
 				onChange={setSelected}
 			/>
 			<RadioItem
 				icon={<Person sx={{ width: '40px', height: '40px' }} />}
-				title={'Community'}
-				description={'The community will take decisions together as a group.'}
-				value={1}
+				title={t('page:organisations:settings:control:radio_button_community:title')}
+				description={t('page:organisations:settings:control:radio_button_community:description')}
+				value={2}
 				selectedValue={selected}
 				onChange={setSelected}
 			/>
