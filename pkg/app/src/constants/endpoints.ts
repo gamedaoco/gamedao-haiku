f7ad7fc65c7d5622813529b18bcb3b4da0a5705c
2075 <1140449+2075@users.noreply.github.com>
2022-11-01 13:54:30 +0100

    chore: auto detect network per environment (#473)
    * feat(detect-env): update images
    
    * feat(detect-env): add versioning, environments, footer note

--- pkg/app/src/constants/endpoints.ts
+++ pkg/app/src/constants/endpoints.ts
@@ -4,48 +4,53 @@ import { Environment } from 'src/queries'
 
 const developmentEndpoints: Endpoints = [
 	{
-		image: '/img/zero.png',
-		name: 'Local env',
+		image: '/svg/z-ctrl-45-wht.svg',
+		name: 'Local Testnet',
 		url: 'http://localhost:9080/v1/graphql',
 		healthCheck: 'http://localhost:9080/healthz',
 		chain: 'wss://localhost:9080',
+	},
+	{
+		image: '/svg/z-ctrl-45-wht.svg',
+		name: 'Rococo Parachain Testnet',
+		url: 'https://graph.rococo.sub.zero.io/v1/graphql',
+		healthCheck: 'https://graph.rococo.sub.zero.io/health',
+		chain: 'wss://node.rococo.sub.zero.io',
 	},
 ]
 
 const productionEndpoints: Endpoints = [
 	{
-		image: '/img/zero.png',
-		name: 'Developent',
+		image: '/svg/z-ctrl-45-wht.svg',
+		name: 'Development',
 		url: 'https://graph.dev.sub.zero.io/v1/graphql',
 		healthCheck: 'https://graph.dev.sub.zero.io/health',
 		chain: 'wss://node.dev.sub.zero.io',
-		default: true,
+		default: ( ENVIRONMENT === Environment.Development ) ? true : false,
 	},
 	{
-		image: '/img/zero.png',
+		image: '/svg/z-ctrl-45-wht.svg',
 		name: 'Staging',
 		url: 'https://graph.stage.sub.zero.io/v1/graphql',
 		healthCheck: 'https://graph.stage.sub.zero.io/health',
 		chain: 'wss://node.stage.sub.zero.io',
+		default: ( ENVIRONMENT === Environment.Staging ) ? true : false,
 	},
 	{
-		image: '/img/zero.png',
-		name: 'Rococo',
-		url: 'https://graph.rococo.sub.zero.io/v1/graphql',
-		healthCheck: 'https://graph.rococo.sub.zero.io/health',
-		chain: 'wss://node.rococo.sub.zero.io',
-	},
-	{
-		image: '/img/zero.png',
+		image: '/svg/z-ctrl-45-wht.svg',
 		name: 'Mainnet',
 		url: 'https://graph.prod.sub.zero.io/v1/graphql',
 		healthCheck: 'https://graph.prod.sub.zero.io/health',
 		chain: 'wss://node.prod.sub.zero.io',
+		default: ( ENVIRONMENT === Environment.Production ) ? true : false,
 	},
 
 ]
 
 export const ENDPOINTS: Endpoints =
-	ENVIRONMENT === Environment.Development ? [...developmentEndpoints, ...productionEndpoints] : productionEndpoints
+	ENVIRONMENT === Environment.Development
+	? [...developmentEndpoints, ...productionEndpoints]
+	: productionEndpoints
 
 export const getConnectedEndpoint = () => ENDPOINTS.find((e) => e.default)
+
