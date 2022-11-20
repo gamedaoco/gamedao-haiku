4994d7e2c89dd2fec111e4074df09db03305a049
2075 <2075@zero.io>
2022-11-20 12:17:06 +0100

    feat: update org enums

---
+++ pkg/app/src/@types/enums.ts
@@ -0,0 +1,17 @@
+export enum AccountTabs {
+	OVERVIEW = 'overview',
+	ORGANIZATIONS = 'organizations',
+	CAMPAIGNS = 'campaigns',
+	COLLECTABLES = 'collectables',
+	IDENTITY = 'identity',
+	SETTINGS = 'settings',
+}
+
+export enum OrganizationTabs {
+	OVERVIEW = 'overview',
+	CAMPAIGNS = 'campaigns',
+	GOVERNANCE = 'governance',
+	MEMBERS = 'members',
+	TREASURY = 'treasury',
+	SETTINGS = 'settings',
+}
--- pkg/app/src/@types/organisation.ts
+++
@@ -1,8 +0,0 @@
-export enum OrganizationTabs {
-	OVERVIEW = 'overview',
-	CAMPAIGNS = 'campaigns',
-	GOVERNANCE = 'governance',
-	MEMEBRS = 'members',
-	TREASURY = 'treasury',
-	SETTINGS = 'settings',
-}
