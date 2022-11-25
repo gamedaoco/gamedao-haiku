f85227dd85dc4172ff6cf780d94e0e89d3780b05
Volodymyr Brazhnyk <crypto@vovacha.com>
2022-11-25 12:30:05 +0100

    fix: self-healing

--- pkg/service/src/resolvers/query/apiProviders/apiProviderResolver.ts
+++ pkg/service/src/resolvers/query/apiProviders/apiProviderResolver.ts
@@ -4,35 +4,32 @@ import { ChainClient } from '../../../modules/chainClient'
 export async function apiProviderResolver(): Promise<ApiProvider> {
 	const chainClient = ChainClient.Instance
 
-	try {
-		if (!chainClient.initialized) {
-			throw new Error('Chain client is not initialized')
-		}
+	if (!chainClient.initialized) {
+		console.log('Chain client is not initialized')
+		process.exit(0)
+	}
 
-		const systemProperties = await chainClient.api.rpc.system.properties()
-		const { tokenDecimals, tokenSymbol, ss58Format } = systemProperties.toHuman()
+	const systemProperties = await chainClient.api.rpc.system.properties()
+	const { tokenDecimals, tokenSymbol, ss58Format } = systemProperties.toHuman()
 
-		const systemName = await chainClient.api.rpc.system.chain()
-		const blockDuration = ((await chainClient.api.call.auraApi.slotDuration()) as any).toNumber() / 1000
+	const systemName = await chainClient.api.rpc.system.chain()
+	const blockDuration = ((await chainClient.api.call.auraApi.slotDuration()) as any).toNumber() / 1000
 
-		return {
-			types: '{}',
-			wsProviderUrl: process.env.CHAIN_RPC_URL_OVERRIDE || chainClient.url,
-			chainProperties: {
-				ss58Format: (ss58Format as any) ?? 25,
-				tokenSymbol: (tokenSymbol as any) ?? ['ZERO'],
-				tokenDecimals: (tokenDecimals as any) ?? [18],
+	return {
+		types: '{}',
+		wsProviderUrl: process.env.CHAIN_RPC_URL_OVERRIDE || chainClient.url,
+		chainProperties: {
+			ss58Format: (ss58Format as any) ?? 25,
+			tokenSymbol: (tokenSymbol as any) ?? ['ZERO'],
+			tokenDecimals: (tokenDecimals as any) ?? [18],
 
-				// TODO: Remove hardcoded values
-				networkCurrency: 0, // 0 = ZERO
-				governanceCurrency: 2, // 2 = GAME
-				paymentCurrencies: 1, // 1 = PLAY
-				blockTargetTime: blockDuration ?? 12,
-			},
-			name: (systemName.toHuman() as any) ?? 'ZERO',
-		}
-	} catch (err) {
-		console.error('apiProviderResolver error', err)
+			// TODO: Remove hardcoded values
+			networkCurrency: 0, // 0 = ZERO
+			governanceCurrency: 2, // 2 = GAME
+			paymentCurrencies: 1, // 1 = PLAY
+			blockTargetTime: blockDuration ?? 12,
+		},
+		name: (systemName.toHuman() as any) ?? 'ZERO',
 	}
 
 	return {} as any
