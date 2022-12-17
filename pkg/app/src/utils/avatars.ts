48e307aa18c8c3cb0179dca2c7e08e6c1dd40163
2075 <1140449+2075@users.noreply.github.com>
2022-12-17 23:36:20 +0100

    feat: update favicon

--- pkg/app/src/utils/avatars.ts
+++ pkg/app/src/utils/avatars.ts
@@ -2,7 +2,6 @@ import md5 from 'md5'
 
 // create an avatar url based on the wallet address
 export const avatarImageURL = (address) => {
-	console.log('address', address)
 	const baseURL = `https://avatars.dicebear.com/api/big-smile/`
 	const avatarHash = md5(address)
 	// const avatarBackground = ${avatarHash.slice(0,6).toUpperCase()}`
