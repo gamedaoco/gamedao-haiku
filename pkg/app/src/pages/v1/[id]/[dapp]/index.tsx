a7c7164d634d2beac417b18ad0edb8221386e880
2075 <1140449+2075@users.noreply.github.com>
2023-01-05 15:14:53 +0100

    feat: update layout, add pages for v1

---
+++ pkg/app/src/pages/v1/[id]/[dapp]/index.tsx
@@ -0,0 +1,32 @@
+import { useRouter } from 'next/router'
+import { useTranslation } from 'react-i18next'
+
+import { DApps } from 'constants/dapps'
+import { useCurrentAccountState } from 'hooks/useCurrentAccountState'
+import { Layout } from 'layouts/v1'
+
+import BattlePass from 'dapps/BattlePass'
+
+import { NoWalletConnected } from 'components/NoWalletConnected/noWalletConnected'
+import { Box, Button, Container, Grid, Typography } from '@mui/material'
+
+export function Page() {
+	const { t } = useTranslation()
+	const { query } = useRouter()
+
+	const id = query?.id as string
+	const dapp = query?.dapp as string
+
+	const walletGate = false
+	const accountState = useCurrentAccountState()
+
+	if (walletGate && !accountState) return <NoWalletConnected />
+
+	return (
+		<Layout showHeader showFooter>
+			<BattlePass id={id} path={dapp as ContentTabs} />
+		</Layout>
+	)
+}
+
+export default Page
