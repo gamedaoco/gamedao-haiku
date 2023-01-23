da7c98ea2aa9b63b1fe83dc2cd9acbb1f4a63daf
2075 <1140449+2075@users.noreply.github.com>
2023-01-23 16:51:59 +0100

    fix: rm page transition

--- pkg/app/src/pages/_app.tsx
+++ pkg/app/src/pages/_app.tsx
@@ -36,7 +36,6 @@ function HeadAndMetaTags() {
 		<Head>
 			<meta
 				name="viewport"
-				// content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no,shrink-to-fit=no"
 				content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=2.0,shrink-to-fit=no"
 			/>
 			<meta name="format-detection" content="telephone=no, address=no, email=no" />
@@ -66,7 +65,6 @@ function HeadAndMetaTags() {
 
 export function MyApp({ Component, emotionCache = clientSideEmotionCache, pageProps }: MyAppProps) {
 	const router = useRouter()
-
 	useEffect(() => {
 		log.info(`❤️  Welcome to GameDAO`)
 		log.info(`💬  Join our discord: https://discord.gg/gamedao`)
@@ -92,9 +90,9 @@ export function MyApp({ Component, emotionCache = clientSideEmotionCache, pagePr
 		<CacheProvider value={emotionCache}>
 			<Providers>
 				<HeadAndMetaTags />
-				<AnimatePresence mode="wait" initial={false}>
-					<Component {...pageProps} key={router.asPath} />
-				</AnimatePresence>
+				{/*				<AnimatePresence mode="wait" initial={false}>*/}
+				<Component {...pageProps} key={router.asPath} />
+				{/*</AnimatePresence>*/}
 			</Providers>
 		</CacheProvider>
 	)
