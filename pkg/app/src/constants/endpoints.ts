02ca8bfe16c667ed7921e98f6eac34d55ad2d217
2075 <1140449+2075@users.noreply.github.com>
2023-01-24 23:41:03 +0100

    fix: disable codegen enumsAsTypes

--- pkg/app/src/constants/endpoints.ts
+++ pkg/app/src/constants/endpoints.ts
@@ -21,7 +21,7 @@ const productionEndpoints: Endpoints = [
 		url: 'https://graph.dev.sub.zero.io/v1/graphql',
 		healthCheck: 'https://graph.dev.sub.zero.io/health',
 		chain: 'wss://node.dev.sub.zero.io',
-		default: env === 'Development' ? true : false,
+		default: ENVIRONMENT === Environment.Development ? true : false,
 	},
 	{
 		image: '/svg/z-ctrl-45-wht.svg',
@@ -29,7 +29,7 @@ const productionEndpoints: Endpoints = [
 		url: 'https://graph.stage.sub.zero.io/v1/graphql',
 		healthCheck: 'https://graph.stage.sub.zero.io/health',
 		chain: 'wss://node.stage.sub.zero.io',
-		default: env === 'Staging' ? true : false,
+		default: ENVIRONMENT === Environment.Staging ? true : false,
 	},
 	{
 		image: '/svg/z-ctrl-45-wht.svg',
@@ -37,11 +37,11 @@ const productionEndpoints: Endpoints = [
 		url: 'https://graph.prod.sub.zero.io/v1/graphql',
 		healthCheck: 'https://graph.prod.sub.zero.io/health',
 		chain: 'wss://node.prod.sub.zero.io',
-		default: env === 'Production' ? true : false,
+		default: ENVIRONMENT === Environment.Production ? true : false,
 	},
 ]
 
 export const ENDPOINTS: Endpoints =
-	env === 'Development' ? [...developmentEndpoints, ...productionEndpoints] : productionEndpoints
+	ENVIRONMENT === Environment.Development ? [...developmentEndpoints, ...productionEndpoints] : productionEndpoints
 
 export const getConnectedEndpoint = () => ENDPOINTS.find((e) => e.default)
