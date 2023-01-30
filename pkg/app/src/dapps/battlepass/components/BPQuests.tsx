c10ba41afcd301c6b3cb18863c1389d79385e04e
2075 <1140449+2075@users.noreply.github.com>
2023-01-30 21:41:42 +0100

    fix: icon filenames lowercase

--- pkg/app/src/dapps/battlepass/components/BPQuests.tsx
+++ pkg/app/src/dapps/battlepass/components/BPQuests.tsx
@@ -28,8 +28,8 @@ type TGridItemProps = {
 
 export const BPQuestItem = ({ index }: TGridItemProps) => {
 	const icons = ['Join', 'Follow', 'Twitter', 'Wallet']
-	const key = icons[index].toLowerCase() // icons[ Math.round( Math.random() * 4 ) ]
-	const url = `/bp/icons/${key}-gold.svg`
+	const key = icons[index] // icons[ Math.round( Math.random() * 4 ) ]
+	const url = `/bp/icons/${key.toLowerCase()}-gold.svg`
 	// eslint-disable-next-line @next/next/no-img-element
 	const Icon = () => <img src={url} height="45px" alt={key} />
 
