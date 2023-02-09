8dfa9b85ba48edf524d445616ea525fc68a9cc86
2075 <1140449+2075@users.noreply.github.com>
2023-02-09 14:03:25 +0100

    feat: add txt to loader

--- pkg/app/src/components/Loader/index.tsx
+++ pkg/app/src/components/Loader/index.tsx
@@ -1,10 +1,16 @@
 import Backdrop from '@mui/material/Backdrop'
-import CircularProgress from '@mui/material/CircularProgress'
+import { CircularProgress, Typography, Stack } from '@mui/material'
 
-export const Loader = () => {
+type Props = {
+	text?: string
+}
+export const Loader = ({ text }: Props) => {
 	return (
 		<Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={true}>
-			<CircularProgress color="inherit" />
+			<Stack spacing={2} justifyContent="center" alignItems="center">
+				<CircularProgress color="inherit" />
+				{text && <Typography variant="h5">{text}</Typography>}
+			</Stack>
 		</Backdrop>
 	)
 }
