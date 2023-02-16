ef79e36be9c445b38ee7eb0e5d35e5ee21f1b43d
2075 <1140449+2075@users.noreply.github.com>
2023-02-16 01:37:10 +0100

    feat: add create battlepass when wallet connected

--- pkg/app/src/pages/battlepass/index.tsx
+++ pkg/app/src/pages/battlepass/index.tsx
@@ -1,8 +1,22 @@
+import { useRouter } from 'next/router'
+import { useTranslation } from 'react-i18next'
+import { useCurrentAccountAddress } from 'hooks/useCurrentAccountAddress'
+
 import { Layout } from 'layouts/v2'
 import { Box, Button, Container, Grid, Typography } from '@mui/material'
 import { Overview } from 'dapps/battlepass'
+import { Add, ArrowDownward } from '@mui/icons-material'
 
 export function Page() {
+	const { t } = useTranslation()
+	const { push } = useRouter()
+	const address = useCurrentAccountAddress()
+
+	const handleCreate = (e) => {
+		if (!address) return
+		push('/battlepass/create')
+	}
+
 	return (
 		<Layout showHeader showFooter>
 			<Box sx={{ mb: 2 }}>
@@ -10,7 +24,13 @@ export function Page() {
 					<Grid item>
 						<Typography variant="h3">Battlepass</Typography>
 					</Grid>
-					<Grid item></Grid>
+					<Grid item>
+						{address && (
+							<Button startIcon={<Add fontSize="small" />} variant="outlined" onClick={handleCreate}>
+								{t('button:ui:create')}
+							</Button>
+						)}
+					</Grid>
 				</Grid>
 				<Grid item>
 					<Typography pb={4} variant="body1" sx={{ maxWidth: { sx: '100%', md: '75%', lg: '50%' } }}>
