19ae8730d7b5541a2ef72a899b41ce7747be5880
2075 <1140449+2075@users.noreply.github.com>
2023-03-22 16:58:33 +0100

    fix: zindex basedialog

--- pkg/app/src/components/BaseDialog/baseDialog.tsx
+++ pkg/app/src/components/BaseDialog/baseDialog.tsx
@@ -13,7 +13,7 @@ interface ComponentProps {
 
 export function BaseDialog({ children, title, open, fullWidth, onClose }: ComponentProps) {
 	return (
-		<Dialog open={open} maxWidth="lg" fullWidth={fullWidth} onClose={onClose}>
+		<Dialog open={open} maxWidth="lg" fullWidth={fullWidth} onClose={onClose} sx={{ zIndex: 16384 }}>
 			<Stack component={Paper} p={4} spacing={4} elevation={5} variant={'glass'}>
 				<Stack direction="row" justifyContent={title ? 'space-between' : 'end'} alignItems="center">
 					{title && <Typography variant="h4">{title}</Typography>}
