001b57e5a4110da037b02a7ce3acea0eda04d1b2
2075 <1140449+2075@users.noreply.github.com>
2023-04-23 18:54:11 +0200

    feat: update comment

--- apps/web/codegen.js
+++ apps/web/codegen.js
@@ -5,10 +5,10 @@ module.exports = {
 			schema: [
 				{
 					'https://graph.dev.sub.zero.io/v1/graphql': {
-						headers: {
-							// "x-hasura-role": "admin",
-							// "x-hasura-admin-secret": "IfRf0ERh7iK4YoZXIr1enMOmVnC2I3fcerldPGck5Q93qwdl7LmP1zCzUyAjKnRy"
-						},
+						// headers: {
+						// "x-hasura-role": "xxxxxxxx",
+						// "x-hasura-admin-secret": "xxxxxxxx"
+						// },
 					},
 				},
 			],
