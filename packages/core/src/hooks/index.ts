9f4c8b28c93f45f25f7f90f6428753b7d5701694
2075 <1140449+2075@users.noreply.github.com>
2024-11-22 01:00:46 +0100

    feat: update hook links

---
+++ packages/core/src/hooks/index.ts
@@ -0,0 +1,12 @@
+export * from './featureToggle/useOrganizationFeatures'
+export * from './featureToggle/useProposalFeatures'
+
+export * from './useAddresses'
+export * from './useApiProvider'
+
+export * from './useAstarDappStakers'
+export * from './useAstarStakedByAddress'
+export * from './useAstarStaking'
+export * from './useAstarTVL'
+
+export * from './useCurrentAccountAddress'
