6d210793f09acad86cb4a79e11b7e9663336037e
2075 <1140449+2075@users.noreply.github.com>
2024-12-13 22:20:19 +0100

    feat(stakeboard): add error handling

--- apps/astar/src/pages/api/rewards/[dapp]/[address].ts
+++ apps/astar/src/pages/api/rewards/[dapp]/[address].ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { ApiPromise, WsProvider } from '@polkadot/api'
+import { isAddress } from '@polkadot/util-crypto'
 
 type ResponseType = {
 	dapp: string
@@ -11,29 +12,45 @@ type ResponseType = {
 	locked: string
 }
 
+type ErrorType = {
+	error: string
+}
+
 const url = 'wss://rpc.astar.network'
 
-export async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+export async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType | ErrorType>) {
 	const { dapp, address } = req.query
 
-	const wsProvider = new WsProvider(url)
-	const api = await ApiPromise.create({ provider: wsProvider })
+	if (!isAddress(address as string)) {
+		res.status(422).json({
+			error: 'bad address input.',
+		})
+	} else {
+		const wsProvider = new WsProvider(url)
+		const api = await ApiPromise.create({ provider: wsProvider })
 
-	const locked = await api.query.dappStaking.ledger(address).then((_) => _.locked.toString())
-	const last = await api.query.dappStaking.ledger(address).then((_) => _.staked.era)
-	const current = await api.query.dappStaking.currentEraInfo().then((_) => _.currentStakeAmount.era)
-	const pending = current - last
-	const batchCount = Math.ceil(pending / 7)
+		try {
+			const locked = await api.query.dappStaking.ledger(address).then((_) => _.locked.toString())
+			const last = await api.query.dappStaking.ledger(address).then((_) => _.staked.era)
+			const current = await api.query.dappStaking.currentEraInfo().then((_) => _.currentStakeAmount.era)
+			const pending = current - last
+			const batchCount = Math.ceil(pending / 7)
 
-	res.status(200).json({
-		dapp: dapp as string,
-		address: address as string,
-		locked: locked,
-		last: last,
-		current: current,
-		pending: pending,
-		batchCount: batchCount,
-	})
+			res.status(200).json({
+				dapp: dapp as string,
+				address: address as string,
+				locked: locked,
+				last: last,
+				current: current,
+				pending: pending,
+				batchCount: batchCount,
+			})
+		} catch (e) {
+			res.status(420).json({
+				error: 'unknown error.',
+			})
+		}
+	}
 }
 
 export default handler
