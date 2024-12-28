389135f58bbea0bbd683f9bb9e482a69db67b6fd
2075 <1140449+2075@users.noreply.github.com>
2024-12-28 10:24:45 +0100

    feat(stakeboard): update $GAME drop calculation

--- apps/astar/src/pages/api/rewards/[dapp]/[address].ts
+++ apps/astar/src/pages/api/rewards/[dapp]/[address].ts
@@ -1,6 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { ApiPromise, WsProvider } from '@polkadot/api'
 import { isAddress } from '@polkadot/util-crypto'
+import BigNumber from 'bignumber.js'
+
+// RewardsType defines parameters needed to calculate
+// a factor, e.g. as multiple of time passed
+// to calculate amounts of token to be dropped.
+// These amounts would be indicative only and
+// should be calculated based on on-chain data.
+
+type RewardsType = {
+	description?: string
+	f?: string
+	totalRewards?: BigNumber
+	claimedRewards?: BigNumber
+	unclaimedRewards?: BigNumber
+	totalEpocs?: number
+}
+
+// Repsonse returns various data related to
+// claimable amounts of $ASTR and a rewards object
+// for custom drop amount calculation.
 
 type ResponseType = {
 	dapp: string
@@ -10,6 +30,7 @@ type ResponseType = {
 	pending: number
 	batchCount: number
 	locked: string
+	rewards: RewardsType | null
 }
 
 type ErrorType = {
@@ -40,6 +61,28 @@ export async function handler(req: NextApiRequest, res: NextApiResponse<Response
 			const pending = current - last
 			const batchCount = Math.ceil(pending / 7)
 
+			// for now we hardwire the calculation of drop rewards
+			// for GameDAO dApp Staking only.
+
+			const description = '$GAME rewards for dApp Stakers'
+			const f = '' // should be pulled from ipfs or onchain
+			const totalRewards = BigNumber(0)
+			const claimedRewards = BigNumber(0)
+			const unclaimedRewards = BigNumber(0)
+			const totalEpocs = 0
+
+			const rewards =
+				dapp === '0x89ed50cec44a3db4186ba54cdf575ec140937c55'
+					? {
+							description: 'claimable GAME rewards',
+							f: '',
+							totalRewards: totalRewards,
+							claimedRewards: claimedRewards,
+							unclaimedRewards: unclaimedRewards,
+							totalEpocs: totalEpocs,
+						}
+					: null
+
 			res.status(200).json({
 				dapp: dapp as string,
 				address: address as string,
@@ -48,6 +91,7 @@ export async function handler(req: NextApiRequest, res: NextApiResponse<Response
 				current: current,
 				pending: pending,
 				batchCount: batchCount,
+				rewards: rewards,
 			})
 		} catch (e) {
 			res.status(420).json({
